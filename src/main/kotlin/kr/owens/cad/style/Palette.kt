bbc6bb30c06137ee532cfc59ad0c30a0c58973de
owen151128 <owen151128@gmail.com>
2022-09-04 22:49:24 +0900

    feat(res): Add colors & resource wrapper

---
+++ src/main/kotlin/kr/owens/cad/style/Palette.kt
@@ -0,0 +1,19 @@
+package kr.owens.cad.style
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * @author owen151128@gmail.com
+ *
+ * Created by owen151128 on 2022/09/04 14:36
+ *
+ * Providing features related to Palette class
+ */
+
+val PrimaryColor = Color(51, 138, 62)
+val Gray = Color.DarkGray
+val Foreground = Color(255, 255, 255)
+val TranslucentBlack = Color(0, 0, 0, 60)
+val Green = Color(102, 187, 106)
+val Yellow = Color(255, 238, 88)
+val Red = Color(239, 83, 80)
