21e185ac4f95cac215f2870ddcb5e9886f0f1284
owen151128 <owen151128@gmail.com>
2022-09-09 03:48:35 +0900

    feat(cache): Add exception for cache not available

---
+++ src/main/kotlin/kr/owens/cad/exception/CacheNotAvailableException.kt
@@ -0,0 +1,10 @@
+package kr.owens.cad.exception
+
+/**
+ * @author owen151128@gmail.com
+ *
+ * Created by owen151128 on 2022/09/09 02:04
+ *
+ * Providing features related to CacheNotAvailableException class
+ */
+class CacheNotAvailableException(message: String) : RuntimeException(message)
